40a0ffb31cbe9e2b59e99e37e1f24745b69d675e
Caeden Statia <caeden.s@outlook.com>
2019-01-26 10:31:33 -0800

    Assembly metadata update to conform to the new ModSaber approval guidelines

--- Counters+/Properties/AssemblyInfo.cs
+++ Counters+/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Counters_")]
+[assembly: AssemblyProduct("CountersPlus")]
 [assembly: AssemblyCopyright("Copyright © Caeden117 2018-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.2.1")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
