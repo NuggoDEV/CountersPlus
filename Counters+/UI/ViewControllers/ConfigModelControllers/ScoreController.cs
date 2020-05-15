e53239b5f0b8d0cb9731390dee5454319912c37a
Caeden Statia <caeden.s@outlook.com>
2020-05-15 14:08:37 -0700

    Finally adding settings for rank colors

--- Counters+/UI/ViewControllers/ConfigModelControllers/ScoreController.cs
+++ Counters+/UI/ViewControllers/ConfigModelControllers/ScoreController.cs
@@ -41,6 +41,55 @@ namespace CountersPlus.UI.ViewControllers.ConfigModelControllers
             set => (parentController?.ConfigModel as ScoreConfigModel).DecimalPrecision = value;
         }
 
+        [UIValue("ss-color")]
+        public Color SSColor
+        {
+            get => GetColorFromHTML((parentController?.ConfigModel as ScoreConfigModel).SSColor);
+            set => (parentController?.ConfigModel as ScoreConfigModel).SSColor = ConvertToHTML(value);
+        }
+
+        [UIValue("s-color")]
+        public Color SColor
+        {
+            get => GetColorFromHTML((parentController?.ConfigModel as ScoreConfigModel).SColor);
+            set => (parentController?.ConfigModel as ScoreConfigModel).SColor = ConvertToHTML(value);
+        }
+
+        [UIValue("a-color")]
+        public Color AColor
+        {
+            get => GetColorFromHTML((parentController?.ConfigModel as ScoreConfigModel).AColor);
+            set => (parentController?.ConfigModel as ScoreConfigModel).AColor = ConvertToHTML(value);
+        }
+
+        [UIValue("b-color")]
+        public Color BColor
+        {
+            get => GetColorFromHTML((parentController?.ConfigModel as ScoreConfigModel).BColor);
+            set => (parentController?.ConfigModel as ScoreConfigModel).BColor = ConvertToHTML(value);
+        }
+
+        [UIValue("c-color")]
+        public Color CColor
+        {
+            get => GetColorFromHTML((parentController?.ConfigModel as ScoreConfigModel).CColor);
+            set => (parentController?.ConfigModel as ScoreConfigModel).CColor = ConvertToHTML(value);
+        }
+
+        [UIValue("d-color")]
+        public Color DColor
+        {
+            get => GetColorFromHTML((parentController?.ConfigModel as ScoreConfigModel).DColor);
+            set => (parentController?.ConfigModel as ScoreConfigModel).DColor = ConvertToHTML(value);
+        }
+
+        [UIValue("e-color")]
+        public Color EColor
+        {
+            get => GetColorFromHTML((parentController?.ConfigModel as ScoreConfigModel).EColor);
+            set => (parentController?.ConfigModel as ScoreConfigModel).EColor = ConvertToHTML(value);
+        }
+
         [UIValue("precision_values")]
         public List<object> PrecisionValues => AdvancedCounterSettings.PercentagePrecision.Cast<object>().ToList();
 
@@ -56,5 +105,13 @@ namespace CountersPlus.UI.ViewControllers.ConfigModelControllers
         {
             parentController.ConfigChanged(obj);
         }
+
+        private Color GetColorFromHTML(string html)
+        {
+            ColorUtility.TryParseHtmlString(html, out Color value);
+            return value;
+        }
+
+        private string ConvertToHTML(Color color) => $"#{ColorUtility.ToHtmlStringRGB(color)}";
     }
 }
