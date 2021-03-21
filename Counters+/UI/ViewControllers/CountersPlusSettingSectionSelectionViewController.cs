e5e429d80fbf35eee15f6f39d1151cc9af7b9099
Caeden117 <caeden.s@outlook.com>
2021-03-20 21:51:55 -0700

    Fix off-center HUD section settings

--- Counters+/UI/ViewControllers/CountersPlusSettingSectionSelectionViewController.cs
+++ Counters+/UI/ViewControllers/CountersPlusSettingSectionSelectionViewController.cs
@@ -1,12 +1,11 @@
-﻿using BeatSaberMarkupLanguage;
-using BeatSaberMarkupLanguage.Attributes;
+﻿using BeatSaberMarkupLanguage.Attributes;
 using BeatSaberMarkupLanguage.Components;
 using BeatSaberMarkupLanguage.ViewControllers;
 using CountersPlus.UI.SettingGroups;
 using HMUI;
 using IPA.Utilities;
-using System;
 using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
 
@@ -14,13 +13,13 @@ namespace CountersPlus.UI.ViewControllers
 {
     public class CountersPlusSettingSectionSelectionViewController : BSMLResourceViewController
     {
-        internal Action<int> OnSettingsGroupChanged;
-
         [Inject] private List<SettingsGroup> loadedSettingsGroups = new List<SettingsGroup>();
 
         [UIComponent("list")] private CustomCellListTableData tableList;
-        [UIComponent("right-button")] private Button leftButton;
-        [UIComponent("left-button")] private Button rightButton;
+        [UIComponent("left-button")] private Button leftButton;
+        [UIComponent("right-button")] private Button rightButton;
+
+        private ScrollView scrollView;
 
         private SettingsGroup selectedGroup;
 
@@ -29,19 +28,41 @@ namespace CountersPlus.UI.ViewControllers
         protected override void DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
         {
             base.DidActivate(firstActivation, addedToHierarchy, screenSystemEnabling);
+
+            scrollView = tableList.tableView.GetField<ScrollView, TableView>("_scrollView");
+            scrollView.scrollPositionChangedEvent += ScrollView_scrollPositionChangedEvent;
+
             HandleCellSelectedEvent(null, (selectedGroup != null) ? loadedSettingsGroups.IndexOf(selectedGroup) : 0);
         }
 
         [UIAction("#post-parse")]
         private void Parsed()
         {
-            var scrollView = tableList.tableView.GetField<ScrollView, TableView>("_scrollView");
-            scrollView.SetField("_pageUpButton", rightButton);
-            scrollView.SetField("_pageDownButton", leftButton);
+            var colors = rightButton.colors;
+            colors.disabledColor = Color.black;
+            rightButton.colors = colors;
+            leftButton.colors = colors;
+        }
+
+        private void ScrollView_scrollPositionChangedEvent(float obj)
+        {
+            // TODO convert into accessors
+            var destinationPos = scrollView.GetField<float, ScrollView>("_destinationPos");
+            var contentSize = scrollView.GetProperty<float, ScrollView>("contentSize");
+            var scrollPageSize = scrollView.GetProperty<float, ScrollView>("scrollPageSize");
+
+            leftButton.interactable = destinationPos > 0.001f;
+            rightButton.interactable = destinationPos < contentSize - scrollPageSize - 0.001f;
+        }
+
+        protected override void DidDeactivate(bool removedFromHierarchy, bool screenSystemDisabling)
+        {
+            base.DidDeactivate(removedFromHierarchy, screenSystemDisabling);
+            scrollView.scrollPositionChangedEvent -= ScrollView_scrollPositionChangedEvent;
         }
 
         [UIAction("option-selected")]
-        protected void OptionSelected(TableView view, CountersPlusListTableCell cell)
+        private void OptionSelected(TableView view, CountersPlusListTableCell cell)
         {
             selectedGroup.OnCellSelect(view, cell.CellIdx);
         }
@@ -68,6 +89,8 @@ namespace CountersPlus.UI.ViewControllers
 
             tableList.tableView.ScrollToCellWithIdx(0, TableView.ScrollPositionType.Beginning, false);
             tableList.tableView.SelectCellWithIdx(selectedGroup.CellToSelect(), false);
+
+            ScrollView_scrollPositionChangedEvent(0f);
         }
     }
 }
