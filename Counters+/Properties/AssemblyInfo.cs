1f269b91a1272cf22736d046544cdfd3261f1688
Caeden117 <caeden.s@outlook.com>
2021-12-03 16:39:05 -0800

    haha almost forgot about assembly info

--- Counters+/Properties/AssemblyInfo.cs
+++ Counters+/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Counters+")]
-[assembly: AssemblyCopyright("Copyright © Caeden117 2018-2020")]
+[assembly: AssemblyCopyright("Copyright © Caeden117 2018-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo(IPA.Config.Stores.GeneratedStore.AssemblyVisibilityTarget)]
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.2")]
-[assembly: AssemblyFileVersion("2.2.2")]
+[assembly: AssemblyVersion("2.2.3")]
+[assembly: AssemblyFileVersion("2.2.3")]
