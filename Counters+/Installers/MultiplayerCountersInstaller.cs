be6722ad65225dbbc64a6186dfbda7e3f8b6837d
Caeden117 <caeden.s@outlook.com>
2022-07-11 14:48:51 -0700

    multiplayer fixes from goob W abber

--- Counters+/Installers/MultiplayerCountersInstaller.cs
+++ Counters+/Installers/MultiplayerCountersInstaller.cs
@@ -10,10 +10,30 @@ namespace CountersPlus.Installers
     {
         public override void InstallBindings()
         {
-            var coreGameHUD = GetComponentInChildren<CoreGameHUDController>();
+            // haha i made goobie beg me for this one line of code
+            if (!Plugin.MainConfig.Enabled) return;
 
-            // We need a separate installer to bind the HUD Controller so counters can use it
-            Container.Bind<CoreGameHUDController>().FromInstance(coreGameHUD).AsSingle();
+            // If CoreGameHUD isn't bound already (by MultEx or others), we need to bind it ourselves
+            var coreGameHUD = Container.TryResolve<CoreGameHUDController>();
+            if (coreGameHUD == null)
+            {
+                // Thankfully the CoreGameHUD is just a child of the installer GameObject
+                coreGameHUD = GetComponentInChildren<CoreGameHUDController>();
+                Container.Bind<CoreGameHUDController>().FromInstance(coreGameHUD).AsSingle();
+
+                // Change base game HUD elements to standard position
+                var energyUIPanel = coreGameHUD.GetComponentInChildren<GameEnergyUIPanel>();
+                energyUIPanel.transform.position = new Vector3(0, -0.64f, 7.75f);
+                energyUIPanel.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+                var comboUI = coreGameHUD.GetComponentInChildren<ComboUIController>();
+                comboUI.transform.position = new Vector3(-3.2f, 1.83f, 7f);
+                comboUI.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+                var multiplierUI = coreGameHUD.GetComponentInChildren<ScoreMultiplierUIController>();
+                multiplierUI.transform.position = new Vector3(3.2f, 1.7f, 7f);
+                multiplierUI.transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
 
             // For the multiplayer rank counter, we also need to bind the Multiplayer Position
             Container.Bind<MultiplayerPositionHUDController>()
@@ -21,19 +41,6 @@ namespace CountersPlus.Installers
                 .AsSingle();
 
             Container.BindInterfacesAndSelfTo<CanvasIntroFadeController>().AsSingle();
-
-            // Change base game HUD elements to standard position
-            var energyUIPanel = coreGameHUD.GetComponentInChildren<GameEnergyUIPanel>();
-            energyUIPanel.transform.position = new Vector3(0, -0.64f, 7.75f);
-            energyUIPanel.transform.rotation = Quaternion.Euler(0, 0, 0);
-
-            var comboUI = coreGameHUD.GetComponentInChildren<ComboUIController>();
-            comboUI.transform.position = new Vector3(-3.2f, 1.83f, 7f);
-            comboUI.transform.rotation = Quaternion.Euler(0, 0, 0);
-
-            var multiplierUI = coreGameHUD.GetComponentInChildren<ScoreMultiplierUIController>();
-            multiplierUI.transform.position = new Vector3(3.2f, 1.7f, 7f);
-            multiplierUI.transform.rotation = Quaternion.Euler(0, 0, 0);
 
             // Install counters like normal
             base.InstallBindings();
