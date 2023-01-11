3c80cc54a53174d81e2819978bb1110c8ef45a44
Goobwabber <47616186+Goobwabber@users.noreply.github.com>
2023-01-11 15:57:55 -0500

    fix 1.27.0 (#129)
    * fix 1.27.0
    
    * bump ver and copyright

--- Counters+/Properties/AssemblyInfo.cs
+++ Counters+/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Counters+")]
-[assembly: AssemblyCopyright("Copyright © Caeden117 2018-2022")]
+[assembly: AssemblyCopyright("Copyright © Caeden117 2018-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo(IPA.Config.Stores.GeneratedStore.AssemblyVisibilityTarget)]
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.1")]
-[assembly: AssemblyFileVersion("2.3.1")]
+[assembly: AssemblyVersion("2.3.2")]
+[assembly: AssemblyFileVersion("2.3.2")]
