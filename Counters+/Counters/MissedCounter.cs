a36d9ea089370da83fec66113c3c055640f1d5f8
RileyPrince <nuggodev@gmail.com>
2024-01-21 00:44:14 +1030

    Updated MissCounter to ignore chains to follow base game combo

--- Counters+/Counters/MissedCounter.cs
+++ Counters+/Counters/MissedCounter.cs
@@ -21,7 +21,7 @@ namespace CountersPlus.Counters
 
         public void OnNoteMiss(NoteData data)
         {
-            if (data.colorType != ColorType.None) counter.text = (++notesMissed).ToString();
+            if (data.colorType != ColorType.None && data.gameplayType != NoteData.GameplayType.BurstSliderElement) counter.text = (++notesMissed).ToString();
         }
     }
 }
